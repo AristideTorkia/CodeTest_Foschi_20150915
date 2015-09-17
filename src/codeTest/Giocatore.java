00b7e542e7a0191c72fb75e249876d6c9d7708af
Bernardo <djfoski@gmail.com>
2015-09-17 14:42:01 +0200

    completamento ultimo junit; aggiornamento readme

--- src/codeTest/Giocatore.java
+++ src/codeTest/Giocatore.java
@@ -7,19 +7,24 @@ package codeTest;
 public class Giocatore {
 	
 	
-	private String sesso;
+//	private String sesso;
 	private String nome;
-	private int vittorie;
-	private int sconfitte;
-	private int pareggi;
-	private int punteggio;
+//	private int vittorie;
+//	private int sconfitte;
+//	private int pareggi;
+//	private int punteggio;
 	
 	public Giocatore() {}
 	
-	public Giocatore(String nome, String sesso) {
+//	public Giocatore(String nome, String sesso) {
+//		super();
+//		this.nome = nome;
+//		this.sesso = sesso;
+//	}
+	
+	public Giocatore(String nome) {
 		super();
 		this.nome = nome;
-		this.sesso = sesso;
 	}
 	
 	public String getNome() {
@@ -28,36 +33,36 @@ public class Giocatore {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public int getVittorie() {
-		return vittorie;
-	}
-	public void setVittorie(int vittorie) {
-		this.vittorie = vittorie;
-	}
-	public int getSconfitte() {
-		return sconfitte;
-	}
-	public void setSconfitte(int sconfitte) {
-		this.sconfitte = sconfitte;
-	}
-	public int getPareggi() {
-		return pareggi;
-	}
-	public void setPareggi(int pareggi) {
-		this.pareggi = pareggi;
-	}
-	public int getPunteggio() {
-		return punteggio;
-	}
-	public void setPunteggio(int punteggio) {
-		this.punteggio = punteggio;
-	}
-	public String getSesso() {
-		return sesso;
-	}
-	public void setSesso(String sesso) {
-		this.sesso = sesso;
-	}
+//	public int getVittorie() {
+//		return vittorie;
+//	}
+//	public void setVittorie(int vittorie) {
+//		this.vittorie = vittorie;
+//	}
+//	public int getSconfitte() {
+//		return sconfitte;
+//	}
+//	public void setSconfitte(int sconfitte) {
+//		this.sconfitte = sconfitte;
+//	}
+//	public int getPareggi() {
+//		return pareggi;
+//	}
+//	public void setPareggi(int pareggi) {
+//		this.pareggi = pareggi;
+//	}
+//	public int getPunteggio() {
+//		return punteggio;
+//	}
+//	public void setPunteggio(int punteggio) {
+//		this.punteggio = punteggio;
+//	}
+//	public String getSesso() {
+//		return sesso;
+//	}
+//	public void setSesso(String sesso) {
+//		this.sesso = sesso;
+//	}
 
 
 	@Override
@@ -74,19 +79,20 @@ public class Giocatore {
 				return false;
 		} else if (!nome.equals(other.nome))
 			return false;
-		if (pareggi != other.pareggi)
-			return false;
-		if (punteggio != other.punteggio)
-			return false;
-		if (sconfitte != other.sconfitte)
-			return false;
-		if (sesso == null) {
-			if (other.sesso != null)
-				return false;
-		} else if (!sesso.equals(other.sesso))
-			return false;
-		if (vittorie != other.vittorie)
-			return false;
+//		if (pareggi != other.pareggi)
+//			return false;
+//		if (punteggio != other.punteggio)
+//			return false;
+//		if (sconfitte != other.sconfitte)
+//			return false;
+//		if (sesso == null) {
+//			if (other.sesso != null)
+//				return false;
+//		} else if (!sesso.equals(other.sesso))
+//			return false;
+//		if (vittorie != other.vittorie)
+//			return false;
+		
 		return true;
 	}
 	
--- src/test/ClasseDiTest.java
+++ src/test/ClasseDiTest.java
@@ -2,6 +2,9 @@ package test;
 
 
 import org.junit.*;
+
+import java.util.Scanner;
+
 import CustomException.CustomException;
 import codeTest.Giocatore;
 import codeTest.Match;
@@ -14,8 +17,8 @@ import codeTest.Match;
 public class ClasseDiTest {
 	
 	private final String nomeDelCampo = "Calcianese";
-	private final Giocatore A = new Giocatore("Bastianic", "M");
-	private final Giocatore B = new Giocatore("Gracco", "M");
+	private final Giocatore A = new Giocatore("Bastianic");
+	private final Giocatore B = new Giocatore("Gracco");
 	
 	
 	@Test
@@ -87,6 +90,33 @@ public class ClasseDiTest {
 	@Test
 	@Ignore
 	public void testSingoloMatch_InputDaUtente() {
+		@SuppressWarnings("resource")
+		Scanner user_input = new Scanner( System.in );
 		
+		System.out.print("Inserisci il nome del campo: ");
+		String campo = user_input.next( );
+		
+		System.out.print("Inserisci il nome del primo Giocatore : ");
+		String nomeGioA = user_input.next( );
+		
+		System.out.print("Inserisci il nome del secondo Giocatore : ");
+		String nomeGioB = user_input.next( );
+		
+		System.out.print("Inserisci la modalita di gioco: corto, intermedio, completo ");
+		String gioco = user_input.next( );
+		
+		Giocatore gioA = new Giocatore(nomeGioA);
+		Giocatore gioB = new Giocatore(nomeGioB);
+		
+		try {
+			Match match = new Match(campo, gioco, gioA , gioB);
+			
+			System.out.println( match.playMatch() );
+			
+			Assert.assertTrue(match.getVincitore().equals(gioA) || match.getVincitore().equals(gioB));
+			
+		} catch (Exception ex) {
+			System.err.println(ex.getMessage());
+		}
 	}
 }
